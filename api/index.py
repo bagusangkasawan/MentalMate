17067ac492a78608a1d2970ffc92b8c33e8288b5
Bagus Angkasawan Sumantri Putra <bagusasp02@gmail.com>
2025-06-17 10:15:43 +0700

    fix error

--- api/index.py
+++ api/index.py
@@ -7,7 +7,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-app = Flask(__name__)
+app = Flask(
+    __name__,
+    template_folder="templates",
+    static_folder="static"
+)
 CORS(app)
 
 # Konfigurasi API Key dan Prompt Sistem
